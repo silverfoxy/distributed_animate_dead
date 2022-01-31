1f702e9e8243a6a11ab2d062569d91022cf9ca3e
silverfoxy <baminazad@cs.stonybrook.edu>
2022-01-31 18:02:35 -0500

    Cleanup

--- php/client/cleanup.sh
+++
@@ -1,3 +0,0 @@
-find ./animate_dead/logs/ -maxdepth 1 -name '*.txt' -delete
-find ./animate_dead/logs/reanimation_logs/ -maxdepth 1 -name '*.txt' -delete
-find ./animate_dead/logs/error_logs/ -maxdepth 1 -name '*.txt' -delete
