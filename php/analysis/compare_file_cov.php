b6b11921f36c64d1996e8065b49984052d9ad3dd
Tsoukaladelis <tsoukchris@gmail.com>
2022-02-22 09:32:04 -0500

    fixed comparison script to work with hotcrp

--- php/analysis/compare_file_cov.php
+++ php/analysis/compare_file_cov.php
@@ -47,7 +47,7 @@ if (isset($entry_point)) {
 
 $lim_covered_files = [];
 foreach($covered_files as $key => $entry) {
-    $lim_covered_files[] = str_replace('/var/www/html/', '', explode(': ', $entry['file_name'])[0]);
+    $lim_covered_files[] = str_replace('/var/www/html/hotcrp/', '', explode(': ', $entry['file_name'])[0]);
 }
 $lim_covered_files = array_unique($lim_covered_files, SORT_STRING);
 // Handle Animate Dead coverage
@@ -58,7 +58,7 @@ $ad_line_coverage = file($ad_line_coverage, FILE_IGNORE_NEW_LINES);
 $ad_covered_files = [];
 foreach ($ad_line_coverage as $line) {
     if (strpos($line, ': ') !== false) {
-        $filename = str_replace('/home/ubuntu/debloating_templates/', '', explode(': ', $line)[0]);
+        $filename = str_replace('/home/ubuntu/debloating_templates/app/', '', explode(': ', $line)[0]);
         $ad_covered_files[] = $filename;
     }
 }
@@ -79,6 +79,7 @@ foreach ($lim_covered_files as $filename) {
 foreach ($ad_covered_files as $filename) {
     if (!in_array($filename, $lim_covered_files)) {
         $extra++;
+	$extra_files[] = $filename;
     }
 }
 
@@ -87,4 +88,8 @@ echo sprintf('%d Matching - %d Extra - %d missing'.PHP_EOL, $matching, $extra, $
 echo 'Missing files'.PHP_EOL;
 foreach ($missing_files as $filename) {
     echo $filename.PHP_EOL;
+}
+echo 'Extra files'.PHP_EOL;
+foreach($extra_files as $filename){
+	echo $filename.PHP_EOL;
 }
