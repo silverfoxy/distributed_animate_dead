27d0b6a9dd575ed773e0cf0b89207e0147a81afc
Silverfoxy <baminazad@cs.stonybrook.edu>
2022-05-12 15:57:25 -0400

    Add cleanup script

---
+++ php/client/cleanup.sh
@@ -0,0 +1,6 @@
+rm -rf animate_dead/logs
+mkdir animate_dead/logs
+mkdir animate_dead/logs/error_logs
+mkdir animate_dead/logs/line_coverage_logs
+mkdir animate_dead/logs/outputs
+mkdir animate_dead/logs/reanimation_logs
