62f1ffb0f051608a7a88c6a317fccbd71aabc960
Silverfoxy <baminazad@cs.stonybrook.edu>
2022-06-27 14:28:19 -0400

    Remove log files from parent repo

--- php/client/vendor/composer/autoload_static.php
+++ php/client/vendor/composer/autoload_static.php
@@ -9,8 +9,8 @@ class ComposerStaticInit38c32169cfb9c35667779d35035d2fc4
     public static $files = array (
         'decc78cc4436b1292c6c0d151b19445c' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
